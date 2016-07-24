99679d8221efd8edbb16241f9da7ebe3d76a246f
tkrex <krex@in.tum.de>
2016-07-24 13:54:43 +0200

    adds google chart to index.html

--- gateway/frontend/js/IDSGateway.js
+++ gateway/frontend/js/IDSGateway.js
@@ -24,7 +24,7 @@ var sampleApp = angular.module('gatewayApp', ["ui.router","ngResource",'uiGmapgo
       });
 
 
-  sampleApp.controller('MainController', ["$scope",'$state','$resource', function($scope, $state, $resource){
+  sampleApp.controller('MainController', ["$scope",'$state','$resource','googlechart', function($scope, $state, $resource){
 
                  var Domains = $resource("rest/domains", {}, {search: {method:"GET", params: {}, isArray: true}});
                      $scope.getDomains = function(){
@@ -36,12 +36,46 @@ var sampleApp = angular.module('gatewayApp', ["ui.router","ngResource",'uiGmapgo
                             			});
                             		};
                        $scope.getDomains();
-          $scope.queryInformation = function() {
+
+      $scope.queryInformation = function() {
             if ($scope.query.location != null) {
             var parsedLocation = parseLocation($scope.query.location);
             }
             $state.go("results",{"domainName": $scope.query.domain, "country":parsedLocation.country,"city":parsedLocation.city,"name": $scope.query.name});
           }
+
+      var chart1 = {};
+      chart1.type = "GeoChart";
+      chart1.data = [
+          ['Locale', 'Count', 'Percent'],
+          ['Germany', 22, 23],
+          ['United States', 34, 11],
+          ['Brazil', 42, 11],
+          ['Canada', 57, 32],
+          ['France', 6, 9],
+          ['RU', 72, 3]
+      ];
+
+      chart1.options = {
+          width: 600,
+          height: 300,
+          chartArea: {left:10,top:10,bottom:0,height:"100%"},
+          colorAxis: {colors: ['#aec7e8', '#1f77b4']},
+          displayMode: 'regions'
+      };
+
+      chart1.formatters = {
+          number : [{
+              columnNum: 1,
+              pattern: "$ #,##0.00"
+          }]
+      };
+
+      $scope.chart = chart1;
+
+
+
+
 
 
           function parseLocation(location) {
