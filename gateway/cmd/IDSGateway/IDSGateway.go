5d2aa5dea9427169a3db87483f0326870cc1f030
tkrex <krex@in.tum.de>
2016-07-25 15:06:26 +0200

    fix gateway cmd file

--- gateway/cmd/IDSGateway/IDSGateway.go
+++ gateway/cmd/IDSGateway/IDSGateway.go
@@ -4,19 +4,19 @@ import (
 	"github.com/tkrex/IDS/gateway/providing"
 	"github.com/tkrex/IDS/gateway/domainControllerManagement"
 
+	"github.com/tkrex/IDS/common/models"
+	"fmt"
 )
 
 func main() {
 
-
+	domainControllerManager := domainControllerManagement.NewDomainControllerManager()
+	if domainController,_ := domainControllerManager.StartNewDomainControllerInstance(models.NewRealWorldDomain("default"),models.NewRealWorldDomain("none")); domainController != nil {
+		fmt.Println("Default DomainController created: ", domainController)
+	}
 	_ = domainControllerManagement.NewDomainContollerManagementInterface("8080")
 
 	providing.NewIDSGatewayWebInterface("8000")
-
-/*	domainControllerManager := domainControllerManagement.NewDomainControllerManager()
-	if domainController,_ := domainControllerManager.StartNewDomainControllerInstance(models.NewRealWorldDomain("default"),models.NewRealWorldDomain("none")); domainController != nil {
-		fmt.Println("Default DomainController created: ", domainController)
-	}*/
 	for  {}
 }
 
